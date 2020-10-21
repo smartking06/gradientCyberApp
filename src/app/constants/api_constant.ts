004c8968aa392255b16c1299bb73fbc8bd834316
smartking06 <sainihimanshu.2011@gmail.com>
2020-10-21 09:31:12 +0530

    Update api_constant.ts

--- src/app/constants/api_constant.ts
+++ src/app/constants/api_constant.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
   baseUrl:
-    'https://my-json-server.typicode.com/smartking06/Gradient-cyber-apis',
+    'https://my-json-server.typicode.com/smartking06/gradientCyberApp',
   posts: '/posts',
   user: '/user',
 };
